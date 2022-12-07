a992239a7ea620ec5482f52300f05bbe9d6badfe
MSeechan <66396292+MSeechan@users.noreply.github.com>
2022-12-07 00:19:17 -0800

    prep for zipping

--- db_project/examples/getindexmax.php
+++
@@ -1,8 +0,0 @@
-<?php
-// CSci 130 - Web Programming
-$mydata=file_get_contents("mydatabase.json");
-// convert the string into JSON object
-$mydata=json_decode($mydata);
-$l=count($mydata); // number of elements in the collection
-echo $l;
-?>
--- db_project/prev_ver/movies.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$json_str = file_get_contents('movies_data.json');       
-echo $json_str; 
-
-?>
